df8f86324c751bb6c1d2e7aac36bf32ae7e9ccba
daelynj <daelyn.jones@juul.com>
2019-05-08 16:52:08 -0700

    Add determine_winner method
    Explicitly show that the game.rb file requires 'deck' and 'CompareHands'.
    
    Create determine_winner method to return a string detailing which hand's
    value is greater.

--- lib/poker_hands/game.rb
+++ lib/poker_hands/game.rb
@@ -1,8 +1,15 @@
+require_relative 'deck'
+require_relative 'CompareHands'
+
 class Game
   def initialize
     deck = Deck.new
 
-    hand1 = deck.draw(5)
-    hand2 = deck.draw(5)
+    @hand1 = deck.draw(5)
+    @hand2 = deck.draw(5)
+  end
+
+  def determine_winner
+    CompareHands.new.call(@hand1, @hand2) ? 'Hand 1 wins' : 'Hand 2 wins'
   end
 end
