46a595e4c10734a4b248a50dd9ef6ba10040cd81
daelynj <daelyn.jones@juul.com>
2019-05-08 09:23:32 -0700

    Use 2 space indentation
    Ruby uses 2 space indentation by convention.

--- lib/poker_hands/card.rb
+++ lib/poker_hands/card.rb
@@ -1,10 +1,10 @@
 class Card
-    def initialize(rank, suit)
-        @rank = rank
-        @suit = suit
-    end
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
 
-    def get_card
-        @rank + @suit
-    end
+  def get_card
+    @rank + @suit
+  end
 end
