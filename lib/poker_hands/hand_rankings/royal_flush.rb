517e51458bcbbbd1487a94fafe79d6b8331f1df7
daelynj <daelyn.jones@juul.com>
2019-05-09 14:59:17 -0700

    Implement RoyalFlush
    Return true if the hand is a royal flush. Return false if the hand is
    not a royal flush. Don't bother checking the ranks of the hand if all
    the suits of the cards aren't the same, thus return false early.

--- lib/poker_hands/hand_rankings/royal_flush.rb
+++ lib/poker_hands/hand_rankings/royal_flush.rb
@@ -1,5 +1,14 @@
 class RoyalFlush
   def check(hand)
-    true
+    royal_flush_ranks = ['A', 'K', 'Q', 'J', '10']
+    ranks_in_hand = []
+    
+    return false if hand.any? { |card| card.suit != hand.first.suit}
+
+    hand.size.times do |card|
+      ranks_in_hand << hand[card].rank
+    end
+
+    (royal_flush_ranks - ranks_in_hand).empty? ? true : false
   end
 end
