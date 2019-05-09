9f55f1659c2dafa313ae18d29a8a519e1824344b
daelynj <daelyn.jones@juul.com>
2019-05-09 12:01:01 -0700

    Add require_relative paths to value
    Add require_relative paths to allow value.rb access to hand_rankings
    files.

--- lib/poker_hands/value.rb
+++ lib/poker_hands/value.rb
@@ -1,3 +1,14 @@
+require_relative 'hand_rankings/royal_flush'
+require_relative 'hand_rankings/straight_flush'
+require_relative 'hand_rankings/four_of_a_kind'
+require_relative 'hand_rankings/full_house'
+require_relative 'hand_rankings/flush'
+require_relative 'hand_rankings/straight'
+require_relative 'hand_rankings/three_of_a_kind'
+require_relative 'hand_rankings/two_pair'
+require_relative 'hand_rankings/pair'
+require_relative 'hand_rankings/high_card'
+
 class Value
   def royal_flush?(hand)
     true
