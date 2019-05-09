bcd6a217f850bdaed762d60e46d9df0bae907b34
daelynj <daelyn.jones@juul.com>
2019-05-09 11:05:11 -0700

    Add retreive_value method
    Add retreive_value method to determine the value of a poker hand using
    the Value class.

--- lib/poker_hands/CompareHands.rb
+++ lib/poker_hands/CompareHands.rb
@@ -1,5 +1,24 @@
+require_relative 'value'
+
 class CompareHands
   def call(hand1, hand2)
-    true
+    h1_val = retreive_value(hand1)
+    h2_val = retreive_value(hand2)
+  end
+
+  def retreive_value(hand)
+    value = [
+              Value.new.royal_flush?(hand),
+              Value.new.straight_flush?(hand),
+              Value.new.four_of_a_kind?(hand),
+              Value.new.full_house?(hand),
+              Value.new.flush?(hand),
+              Value.new.straight?(hand),
+              Value.new.three_of_a_kind?(hand),
+              Value.new.two_pair?(hand),
+              Value.new.pair?(hand),
+              Value.new.high_card?(hand)
+            ]
+    value.max
   end
 end
