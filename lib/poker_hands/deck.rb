f654849152d430f638f4551862290bc79f34f6fc
daelynj <daelyn.jones@juul.com>
2019-05-10 09:18:48 -0700

    Change J, Q, K, A ranks to integers
    Allow us to sort poker hands by integers without having to deal with
    sorting ascii characters somehow.

--- lib/poker_hands/deck.rb
+++ lib/poker_hands/deck.rb
@@ -2,7 +2,7 @@ require_relative 'card'
 
 class Deck
   def initialize
-    @ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    @ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14']
     @suits = ['C', 'H', 'S', 'D']
     @cards = []
 
