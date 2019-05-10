104a400724d216a99254d2084c0216fe778637e5
daelynj <daelyn.jones@juul.com>
2019-05-10 13:49:32 -0700

    Implement FourOfAKind
    return true if the hand is a four of a kind. Return false if the hand
    is not.

--- lib/poker_hands/hand_rankings/four_of_a_kind.rb
+++ lib/poker_hands/hand_rankings/four_of_a_kind.rb
@@ -1,5 +1,7 @@
 class FourOfAKind
   def check(hand)
-    true
+    hand.sort_by! { |card| card.rank.to_i }
+    
+    hand.each_cons(4).any? { |card| card.uniq { |card| card.rank}.length == 1 }
   end
 end
