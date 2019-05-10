d0d53d4713e0e21f156727087b825222ea1cc8f0
daelynj <daelyn.jones@juul.com>
2019-05-10 09:23:06 -0700

    Change ranks to represent integers
    The previous commit requires that all royal ranks of cards be
    represented as integers

--- lib/poker_hands/hand_rankings/royal_flush.rb
+++ lib/poker_hands/hand_rankings/royal_flush.rb
@@ -1,6 +1,6 @@
 class RoyalFlush
   def check(hand)
-    royal_flush_ranks = ['A', 'K', 'Q', 'J', '10']
+    royal_flush_ranks = ['10', '11', '12', '13', '14']
     
     return false if hand.any? { |card| card.suit != hand.first.suit}
 
