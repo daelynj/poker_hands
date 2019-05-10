66854c73525c57ea28216985338cbb00d1f8fa68
daelynj <daelyn.jones@juul.com>
2019-05-10 14:34:31 -0700

    Clean up FourOfAKind
    The previous commit of this logic was unclear and difficult to read.
    This is much nicer to read.

--- lib/poker_hands/hand_rankings/four_of_a_kind.rb
+++ lib/poker_hands/hand_rankings/four_of_a_kind.rb
@@ -1,7 +1,12 @@
 class FourOfAKind
   def check(hand)
     hand.sort_by! { |card| card.rank.to_i }
-    
-    hand.each_cons(4).any? { |card| card.uniq { |card| card.rank}.length == 1 }
+    #hand.each_cons(4).any? { |card| card.uniq { |card| card.rank}.length == 1 }
+
+    ranks_in_hand = hand.map(&:rank)
+
+    ranks_in_hand.uniq.any? do |rank|
+      ranks_in_hand.count(rank) == 4
+    end
   end
 end
