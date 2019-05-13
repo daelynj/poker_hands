2d2696ca6d3fc9457d3f485ac55ba82ad35a0e36
daelynj <daelyn.jones@juul.com>
2019-05-13 15:26:15 -0700

    Add spec for Straight
    Check if the Straight service object returns true when given a hand
    containing a Straight. Also check that a hand that does not contain a
    straight returns false.

---
+++ spec/straight_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe Straight do
+  describe '.check' do
+    subject { Straight.new.check(hand) }
+
+    context 'when hand is a Straight' do
+      let(:hand) do
+        [
+          Card.new('9', 'C'),
+          Card.new('8', 'D'),
+          Card.new('7', 'S'),
+          Card.new('6', 'D'),
+          Card.new('5', 'H')
+        ]
+      end
+
+      it { is_expected.to be true }
+    end
+
+    context 'when hand is not a Straight' do
+      let(:hand) do
+        [
+          Card.new('7', 'C'),
+          Card.new('7', 'D'),
+          Card.new('7', 'S'),
+          Card.new('13', 'C'),
+          Card.new('3', 'D')
+        ]
+      end
+
+      it { is_expected.to be false }
+    end
+  end
+end
