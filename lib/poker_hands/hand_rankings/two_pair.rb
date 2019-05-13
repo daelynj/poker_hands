ab448828d11ac0feceac22208e4bb0e1e8db238e
daelynj <daelyn.jones@juul.com>
2019-05-13 10:02:18 -0700

    Clean up TwoPair
    New logic is much easier to read and performs the same.

--- lib/poker_hands/hand_rankings/two_pair.rb
+++ lib/poker_hands/hand_rankings/two_pair.rb
@@ -2,8 +2,6 @@ class TwoPair
   def check(hand)
     ranks_in_hand = hand.map(&:rank)
 
-    pairs = ranks_in_hand.find_all { |rank| ranks_in_hand.count(rank) > 1 }
-
-    pairs.length > 2
+    ranks_in_hand.uniq.count { |rank| ranks_in_hand.count(rank) > 1 } == 2
   end
 end
