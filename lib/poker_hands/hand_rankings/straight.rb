d6bc2d708abe4473919243e4f56209997851df42
daelynj <daelyn.jones@juul.com>
2019-05-13 10:34:23 -0700

    Implement Straight
    Almost the same implementation as StraightFlush except we check if all
    of the suits are the same. If they are the same, we return early because
    we might have a StraightFlush (all of the suits are the same).
    
    We then check if the cards in sequence by sorting and checking if
    every card is delta 1 of each other.

--- lib/poker_hands/hand_rankings/straight.rb
+++ lib/poker_hands/hand_rankings/straight.rb
@@ -1,5 +1,8 @@
 class Straight
   def check(hand)
-    true
+    return false if hand.all? { |card| card.suit == hand.first.suit}
+    hand.sort_by! { |card| card.rank.to_i }
+
+    hand.each_cons(2).all? { |card, next_card| next_card.rank.to_i == card.rank.to_i + 1 }
   end
 end
