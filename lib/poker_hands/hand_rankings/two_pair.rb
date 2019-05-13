707feece4fe1fc4cb0180513c3ff671c7bab33fd
daelynj <daelyn.jones@juul.com>
2019-05-13 09:48:22 -0700

    Implement TwoPair
    TwoPair determines if a 5 card poker hand contains two pairs of cards.
    We separate the pairs out of the poker hand into their own array
    and check if the length of the array implies we have two pairs, thus
    returning true if so and false otherwise.

--- lib/poker_hands/hand_rankings/two_pair.rb
+++ lib/poker_hands/hand_rankings/two_pair.rb
@@ -1,5 +1,9 @@
 class TwoPair
   def check(hand)
-    true
+    ranks_in_hand = hand.map(&:rank)
+
+    pairs = ranks_in_hand.find_all { |rank| ranks_in_hand.count(rank) > 1 }
+
+    pairs.length > 2
   end
 end
