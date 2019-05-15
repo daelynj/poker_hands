885072735c39255991b4417197b41b8c894dd840
daelynj <daelyn.jones@juul.com>
2019-05-14 17:03:43 -0700

    Add a basic example of the new logic working
    Test two poker hands and print which hand wins.

--- lib/poker_hands/compare_hands.rb
+++ lib/poker_hands/compare_hands.rb
@@ -1,32 +1,47 @@
-require_relative 'value'
+require_relative 'hand_rankings/find_pair'
 
 module PokerHands
   class CompareHands
     def call(hand1, hand2)
-      h1_val = retrieve_value(hand1)
-      h2_val = retrieve_value(hand2)
+      hand1 =
+      [
+        Card.new('13', 'H'),
+        Card.new('13', 'D'),
+        Card.new('9', 'C'),
+        Card.new('4', 'S'),
+        Card.new('7', 'H')
+      ]
+      hand2 =
+      [
+        Card.new('13', 'H'),
+        Card.new('13', 'D'),
+        Card.new('8', 'C'),
+        Card.new('4', 'S'),
+        Card.new('7', 'H')        
+      ]
 
-      winner(h1_val, h2_val)
-    end
+      result = FindPair.new.call(hand1) <=> FindPair.new.call(hand2)
 
-    def retrieve_value(hand)
-      value = [
-                Value.new.royal_flush?(hand),
-                Value.new.straight_flush?(hand),
-                Value.new.four_of_a_kind?(hand),
-                Value.new.full_house?(hand),
-                Value.new.flush?(hand),
-                Value.new.straight?(hand),
-                Value.new.three_of_a_kind?(hand),
-                Value.new.two_pair?(hand),
-                Value.new.pair?(hand),
-                Value.new.high_card?(hand)
-              ]
-      value.index(true)
-    end
-    
-    def winner(h1_val, h2_val)
-      h1_val < h2_val ? 'h1' : 'h2'
+      if (result) == 1
+        puts 'hand 1 wins'
+      elsif (result) == -1
+        puts 'hand 2 wins'
+      else
+        puts 'hands are equal'
+      end
     end
   end
 end
+
+=begin
+  Value.new.royal_flush?(hand),
+  Value.new.straight_flush?(hand),
+  Value.new.four_of_a_kind?(hand),
+  Value.new.full_house?(hand),
+  Value.new.flush?(hand),
+  Value.new.straight?(hand),
+  Value.new.three_of_a_kind?(hand),
+  Value.new.two_pair?(hand),
+  Value.new.pair?(hand),
+  Value.new.high_card?(hand)
+=end
