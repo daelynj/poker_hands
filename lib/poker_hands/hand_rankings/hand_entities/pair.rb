903bd36c044f71eb8125f079e4a6fc807d4533a9
daelynj <daelyn.jones@juul.com>
2019-05-15 09:50:21 -0700

    Update find_pair and pair
    Remove proof of concept code.

--- lib/poker_hands/hand_rankings/hand_entities/pair.rb
+++ lib/poker_hands/hand_rankings/hand_entities/pair.rb
@@ -2,19 +2,20 @@ module PokerHands
   module Entities
     class Pair
       include Comparable
-      attr_reader :pair, :other_cards
+      attr_reader :pair, :other_cards, :strength
 
       def initialize(pair:, other_cards:)
         @pair = pair.map(&:rank)
         @other_cards = other_cards.map(&:rank).reverse
+        @strength = 2
       end
 
-      def <=>(other_hand)
-        pair <=> other_hand.pair
-        if (pair <=> other_hand.pair) == 0
-          return other_cards <=> other_hand.other_cards
-        end
-      end
+      #def <=>(other_hand)
+      #  pair <=> other_hand.pair
+      #  if (pair <=> other_hand.pair) == 0
+      #    return other_cards <=> other_hand.other_cards
+      #  end
+      #end
     end
   end
 end
