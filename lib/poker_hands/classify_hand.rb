f0aaa226324e5443c160a3401a3af13aad35ff24
daelynj <daelyn.jones@juul.com>
2019-05-15 14:01:17 -0700

    Implement FindFourOfAKind
    FindFourOfAKind returns early if the hand does not contain a
    FourOfAKind, otherwise return a FourOfAKind entity.

--- lib/poker_hands/classify_hand.rb
+++ lib/poker_hands/classify_hand.rb
@@ -4,6 +4,7 @@ require_relative 'hand_rankings/find_high_card'
 require_relative 'hand_rankings/find_three_of_a_kind'
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 require_relative 'hand_rankings/find_straight_flush'
 require_relative 'hand_rankings/find_flush'
 =======
@@ -12,12 +13,16 @@ require_relative 'hand_rankings/find_straight'
 =======
 require_relative 'hand_rankings/find_full_house'
 >>>>>>> Implement FindFullHouse
+=======
+require_relative 'hand_rankings/find_four_of_a_kind'
+>>>>>>> Implement FindFourOfAKind
 
 module PokerHands
   class ClassifyHand
     def call(hand)
       found_hands = 
       [
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
         FindStraightFlush.new.call(hand),
@@ -28,6 +33,9 @@ module PokerHands
 =======
         FindFullHouse.new.call(hand),
 >>>>>>> Implement FindFullHouse
+=======
+        FindFourOfAKind.new.call(hand),
+>>>>>>> Implement FindFourOfAKind
         FindThreeOfAKind.new.call(hand),
         FindTwoPair.new.call(hand),
         FindPair.new.call(hand),
