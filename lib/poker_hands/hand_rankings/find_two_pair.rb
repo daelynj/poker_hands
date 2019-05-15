226d0fcd2359d879f2f9c3bc03ff3c588dae60ac
daelynj <daelyn.jones@juul.com>
2019-05-15 09:48:29 -0700

    Implement two_pair entity and find_two_pair
    The FindTwoPair class takes a hand and puts the two pairs in a hand into
    an array, and the left over card into an array. Returns early if there
    are not two pairs.
    
    The TwoPair class holds onto the two pairs and left over card, to later
    be used.

---
+++ lib/poker_hands/hand_rankings/find_two_pair.rb
@@ -0,0 +1,19 @@
+require_relative 'hand_entities/two_pair'
+
+module PokerHands
+  class FindTwoPair
+    def call(hand)
+      ranks_in_hand = hand.map(&:rank)
+
+      if ranks_in_hand.uniq.count { |rank| ranks_in_hand.count(rank) > 1 } < 2
+        return nil
+      end
+
+      pairs_ranks = ranks_in_hand.select { |rank| ranks_in_hand.count(rank) == 2 }.uniq
+      pairs = hand.select { |card| card.rank == pairs_ranks[0] || card.rank == pairs_ranks[1] }
+      other_cards = hand.select { |card| card.rank != pairs_ranks[0] && card.rank != pairs_ranks[1] }
+      
+      Entities::TwoPair.new(pairs: pairs, other_cards: other_cards)
+    end
+  end
+end
