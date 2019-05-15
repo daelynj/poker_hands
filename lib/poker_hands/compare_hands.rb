b5db5ccf5e15e37afbaf5aa49cddc77a32495b12
daelynj <daelyn.jones@juul.com>
2019-05-15 11:07:12 -0700

    Refactor CompareHands
    CompareHands now passes hands to ClassifyHand to receive Entities
    that represent the poker hand inside that hand and will eventually
    compare the hands.

--- lib/poker_hands/compare_hands.rb
+++ lib/poker_hands/compare_hands.rb
@@ -1,15 +1,27 @@
-require_relative 'hand_rankings/find_pair'
-require_relative 'hand_rankings/find_two_pair'
+require_relative 'classify_hand'
 
 module PokerHands
   class CompareHands
     def call(hand1, hand2)
+      @hand1 = hand1
+      @hand2 = hand2
+
+      hand1 = ClassifyHand.new.call(@hand1)
+      hand2 = ClassifyHand.new.call(@hand2)
+
+      #hand1.strength < hand2.strength
+    end
+  end
+end
+
+#test hands
+=begin
       @hand1 =
       [
-        Card.new('13', 'H'),
+        Card.new('14', 'H'),
         Card.new('13', 'D'),
         Card.new('12', 'C'),
-        Card.new('12', 'S'),
+        Card.new('11', 'S'),
         Card.new('7', 'H')
       ]
       @hand2 =
@@ -20,12 +32,4 @@ module PokerHands
         Card.new('4', 'S'),
         Card.new('7', 'H')        
       ]
-
-      hand1 = ClassifyHand.new.call(hand1)
-      hand2 = ClassifyHand.new.call(hand2)
-
-      #hand1.strength < hand2.strength
-      
-    end
-  end
-end
+=end
