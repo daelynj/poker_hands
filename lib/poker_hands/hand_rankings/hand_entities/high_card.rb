bbe4104b10a21cf170d2af936278cba84b17d606
daelynj <daelyn.jones@juul.com>
2019-05-15 16:17:27 -0700

    Implement Hand tie scenario
    When both poker hands are a HighCard, using Comparable, the HighCard
    entity will return the winner. If both hands are exactly alike, 'tie' is
    returned.
    Very open to syntax suggestions here to make this easier to read.
    
    Clean up high-card.rb, very open to syntax suggestions here.

--- lib/poker_hands/hand_rankings/hand_entities/high_card.rb
+++ lib/poker_hands/hand_rankings/hand_entities/high_card.rb
@@ -5,8 +5,16 @@ module PokerHands
       attr_reader :cards, :strength
 
       def initialize(cards:)
-        @cards = cards
+        @cards = cards.sort! { |card| card.rank }.map(&:rank)
         @strength = 1
+      end
+
+      def <=>(other_hand)
+        if (@cards <=> other_hand.cards) != 0
+          return @cards <=> other_hand.cards
+        elsif (@cards <=> other_hand.cards) == 0
+          return 'tie'
+        end
       end
     end
   end
