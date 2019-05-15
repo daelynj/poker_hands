27ca5b40bdc99d5ce35986c0772da87ad49ffe16
daelynj <daelyn.jones@juul.com>
2019-05-15 13:32:16 -0700

    Implement FindStraight
    FindStraight returns early if the hand does not contain a Straight,
    otherwise return a Straight entity.

--- lib/poker_hands/classify_hand.rb
+++ lib/poker_hands/classify_hand.rb
@@ -2,16 +2,24 @@ require_relative 'hand_rankings/find_two_pair'
 require_relative 'hand_rankings/find_pair'
 require_relative 'hand_rankings/find_high_card'
 require_relative 'hand_rankings/find_three_of_a_kind'
+<<<<<<< HEAD
 require_relative 'hand_rankings/find_straight_flush'
 require_relative 'hand_rankings/find_flush'
+=======
+require_relative 'hand_rankings/find_straight'
+>>>>>>> Implement FindStraight
 
 module PokerHands
   class ClassifyHand
     def call(hand)
       found_hands = 
       [
+<<<<<<< HEAD
         FindStraightFlush.new.call(hand),
         FindFlush.new.call(hand),
+=======
+        FindStraight.new.call(hand),
+>>>>>>> Implement FindStraight
         FindThreeOfAKind.new.call(hand),
         FindTwoPair.new.call(hand),
         FindPair.new.call(hand),
