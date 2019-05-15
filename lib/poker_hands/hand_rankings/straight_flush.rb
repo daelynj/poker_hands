00355b3601fb5ba4a4f6b51a8e424e4684e41680
daelynj <daelyn.jones@juul.com>
2019-05-15 14:13:20 -0700

    Implement StraightFlush
    StraightFlush is an entity that contains the strength of a StraightFlush
    and the cards of a StraightFlush.

--- lib/poker_hands/hand_rankings/straight_flush.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'flush'
-require_relative 'straight'
-module PokerHands
-  class StraightFlush
-    def check(hand)
-      Flush.new.check(hand) && Straight.new.check(hand)
-    end
-  end
-end
