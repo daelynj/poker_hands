73628c9e8ffbb8c9bf046408a93311785721d15f
daelynj <daelyn.jones@juul.com>
2019-05-15 13:47:38 -0700

    Implement FindFullHouse
    FindFullHouse returns early if the hand does not contain a ThreeOfAKind,
    otherwise return a FullHouse entity.

--- lib/poker_hands/classify_hand.rb
+++ lib/poker_hands/classify_hand.rb
@@ -3,11 +3,15 @@ require_relative 'hand_rankings/find_pair'
 require_relative 'hand_rankings/find_high_card'
 require_relative 'hand_rankings/find_three_of_a_kind'
 <<<<<<< HEAD
+<<<<<<< HEAD
 require_relative 'hand_rankings/find_straight_flush'
 require_relative 'hand_rankings/find_flush'
 =======
 require_relative 'hand_rankings/find_straight'
 >>>>>>> Implement FindStraight
+=======
+require_relative 'hand_rankings/find_full_house'
+>>>>>>> Implement FindFullHouse
 
 module PokerHands
   class ClassifyHand
@@ -15,11 +19,15 @@ module PokerHands
       found_hands = 
       [
 <<<<<<< HEAD
+<<<<<<< HEAD
         FindStraightFlush.new.call(hand),
         FindFlush.new.call(hand),
 =======
         FindStraight.new.call(hand),
 >>>>>>> Implement FindStraight
+=======
+        FindFullHouse.new.call(hand),
+>>>>>>> Implement FindFullHouse
         FindThreeOfAKind.new.call(hand),
         FindTwoPair.new.call(hand),
         FindPair.new.call(hand),
