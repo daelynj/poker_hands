289344b59f0c2ae3c3269f5a58d5de4c9788733a
daelynj <daelyn.jones@juul.com>
2019-05-16 09:46:25 -0700

    Implement Straight tie scenario
    When both poker hands are a Straight, using Comparable, the Straight
    entity will return the winner. If both hands are exactly alike, 'tie' is
    returned.
    Very open to syntax suggestions here to make this easier to read.

--- lib/poker_hands/hand_rankings/find_straight.rb
+++ lib/poker_hands/hand_rankings/find_straight.rb
@@ -3,12 +3,12 @@ require_relative 'hand_entities/straight'
 module PokerHands
   class FindStraight
     def call(hand)
-      hand.sort_by! { |card| card.rank.to_i }
+      hand.sort_by! { |card| card.rank }
 
-      if !hand.each_cons(2).all? { |card, next_card| next_card.rank.to_i == card.rank.to_i + 1 }
+      if !hand.each_cons(2).all? { |card, next_card| next_card.rank == card.rank + 1 }
         return nil
       end
-
+      
       Entities::Straight.new(straight: hand)
     end
   end
--- lib/poker_hands/hand_rankings/hand_entities/straight.rb
+++ lib/poker_hands/hand_rankings/hand_entities/straight.rb
@@ -5,8 +5,15 @@ module PokerHands
       attr_reader :straight, :strength
 
       def initialize(straight:)
-        @straight = straight.map(&:rank)
+        @straight = straight.map(&:rank).reverse
         @strength = 5
+      end
+      def <=>(other_hand)
+        if (@straight <=> other_hand.straight) != 0
+          return @straight <=> other_hand.straight
+        else
+          return 'tie'
+        end
       end
     end
   end
