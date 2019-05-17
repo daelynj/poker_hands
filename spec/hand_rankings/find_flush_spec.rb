146e72327b145c722a1635fd32ad1499305821fc
daelynj <daelyn.jones@juul.com>
2019-05-17 10:16:37 -0700

    Test FindFlush service object
    We need to verify that we are getting an instance of the Flush entity
    because FindFlush returns that entity, which is used to determine the
    winner if a hand contains a flush.
    
    We need to verify the :flush attribute on the Flush entity because we
    cannot perform comparisons during a tie without properly saving the
    Card objects provided when a flush is identified.
    
    We need to verify that FindFlush only returns a Flush entity when a
    flush is found because misidentifying a poker hand as a flush would
    result in innacurate indentification of a winner.

--- spec/hand_rankings/find_flush_spec.rb
+++ spec/hand_rankings/find_flush_spec.rb
@@ -9,20 +9,33 @@ RSpec.describe PokerHands::FindFlush do
     context 'when hand is a Flush' do
       let(:hand) do
         [
-          PokerHands::Card.new('4', 'S'),
-          PokerHands::Card.new('11', 'S'),
-          PokerHands::Card.new('8', 'S'),
-          PokerHands::Card.new('2', 'S'),
-          PokerHands::Card.new('9', 'S')
+          PokerHands::Card.new(4, 'S'),
+          PokerHands::Card.new(11, 'S'),
+          PokerHands::Card.new(8, 'S'),
+          PokerHands::Card.new(2, 'S'),
+          PokerHands::Card.new(9, 'S')
         ]
       end
 
       it { is_expected.to be_an_instance_of(PokerHands::Entities::Flush) }
 
-      #it 'returns the expected cards in the flush' do
-      #  binding.pry
-        #expect(subject.flush).to 
-      #end
+      it 'returns the expected cards in the flush' do
+        expect(subject.flush).to be(hand)
+      end
+    end
+    
+    context 'when hand is not a Flush' do
+      let (:hand) do
+        [
+          PokerHands::Card.new(4, 'H'),
+          PokerHands::Card.new(11, 'S'),
+          PokerHands::Card.new(8, 'S'),
+          PokerHands::Card.new(2, 'D'),
+          PokerHands::Card.new(9, 'S')
+        ]
+      end
+
+      it { is_expected.to be_nil }
     end
   end
 end
