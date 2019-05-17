93d19dcba5dc6fcd437355ea209ab0448d2db790
daelynj <daelyn.jones@juul.com>
2019-05-17 11:13:14 -0700

    Test FindHighCard service object
    We need to verify that we are getting an instance of the HighCard entity
    because FindHighCard returns that entity, which is used to determine the
    winner if a hand contains no better poker hand.
    
    We need to verify the :cards attribute on the HighCard entity because we
    cannot perform comparisons during a tie without properly saving the
    Card objects provided when a HighCard is identified.

---
+++ spec/hand_rankings/find_high_card_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../../lib/poker_hands/hand_rankings/find_high_card'
+require_relative '../../lib/poker_hands/card'
+require 'pry'
+
+RSpec.describe PokerHands::FindHighCard do
+  describe '#call' do
+    subject { PokerHands::FindHighCard.new.call(hand) }
+
+    context 'when hand is a HighCard' do
+      let(:hand) do
+        [
+          PokerHands::Card.new(4, 'H'),
+          PokerHands::Card.new(11, 'S'),
+          PokerHands::Card.new(8, 'D'),
+          PokerHands::Card.new(2, 'S'),
+          PokerHands::Card.new(9, 'C')
+        ]
+      end
+
+      it { is_expected.to be_an_instance_of(PokerHands::Entities::HighCard) }
+
+      it 'returns the expected cards in the high card' do
+        expect(subject.cards).to be(hand)
+      end
+    end
+  end
+end
