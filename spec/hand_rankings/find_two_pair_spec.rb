e81731f9c7f882625cf8baff550fabeff10e467d
daelynj <daelyn.jones@juul.com>
2019-05-17 13:25:17 -0700

    Spec for FindTwoPair service object
    We need to verify that we are getting an instance of the TwoPair entity
    because FindTwoPair returns that entity, which is used to determine the
    winner if a hand contains a two pair.
    
    We need to verify the :cards, :pairs, :other_cards attributes on the
    TwoPair entity because we cannot perform comparisons during a tie
    without properly saving the Card objects provided when a two pair is
    identified.
    
    We need to verify that FindTwoPair only returns a TwoPair entity when a
    two pair is found because misidentifying a poker hand as a two pair
    would result in inaccurate identification of a winner.

---
+++ spec/hand_rankings/find_two_pair_spec.rb
@@ -0,0 +1,56 @@
+require_relative '../../lib/poker_hands/hand_rankings/find_two_pair'
+require_relative '../../lib/poker_hands/card'
+require 'pry'
+
+RSpec.describe PokerHands::FindTwoPair do
+  describe '#call' do
+    subject { PokerHands::FindTwoPair.new.call(hand) }
+
+    context 'when hand is a TwoPair' do
+      let(:pair_cards) do
+        [
+          PokerHands::Card.new(11, 'D'), 
+          PokerHands::Card.new(11, 'S'),
+          PokerHands::Card.new(4, 'H'),
+          PokerHands::Card.new(4, 'C')
+        ]
+      end
+
+      let(:other_card) do
+        [
+          PokerHands::Card.new(9, 'H')
+        ]
+      end
+
+      let(:hand) { pair_cards + other_card }
+
+      it { is_expected.to be_an_instance_of(PokerHands::Entities::TwoPair) }
+
+      it 'returns the expected cards in the hand' do
+        expect(subject.cards).to be(hand)
+      end
+
+      it 'returns the expected cards in the pair' do
+        expect(subject.pairs).to match_array(pair_cards)
+      end
+
+      it 'returns the expected cards in the other cards' do
+        expect(subject.other_card).to match_array(other_card)
+      end
+
+      context 'when hand is not a Pair' do
+        let(:hand) do
+          [
+            PokerHands::Card.new(4, 'H'),
+            PokerHands::Card.new(11, 'S'),
+            PokerHands::Card.new(8, 'S'),
+            PokerHands::Card.new(2, 'D'),
+            PokerHands::Card.new(9, 'S')
+          ]
+        end
+
+        it { is_expected.to be_nil }
+      end
+    end
+  end
+end
