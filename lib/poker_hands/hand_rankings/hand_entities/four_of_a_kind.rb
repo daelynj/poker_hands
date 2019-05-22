77395662619309ea87e80bb6348b991fb4f7b940
daelynj <daelyn.jones@juul.com>
2019-05-22 15:27:31 -0700

    Remove tie scenario from FourOfAKind
    it's impossible for two poker hands to have the same four of a kind so
    we do not need to check the 'other cards' in a four of a kind hand and
    also don't need to check if two quads are the same, as there are no 8 of
    one card.

--- lib/poker_hands/hand_rankings/hand_entities/four_of_a_kind.rb
+++ lib/poker_hands/hand_rankings/hand_entities/four_of_a_kind.rb
@@ -14,18 +14,9 @@ module PokerHands
 
       def <=>(other_hand)
         quads = @quads.map(&:rank).uniq
-        other_card = @other_card.map(&:rank)
-
         other_hand_quads = other_hand.quads.map(&:rank).uniq
-        other_hand_card = other_hand.other_card.map(&:rank)
 
-        if (quads <=> other_hand_quads) != 0
-          return quads <=> other_hand_quads
-        elsif (other_card <=> other_hand_card) != 0
-          return other_card <=> other_hand_card
-        else
-          return 'tie'
-        end
+        quads <=> other_hand_quads
       end
     end
   end
