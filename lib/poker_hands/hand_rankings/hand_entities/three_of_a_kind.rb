7b04b2d806df450418272d8b287e9bed435d650c
daelynj <daelyn.jones@juul.com>
2019-05-22 11:53:45 -0700

    Remove tie scenario from ThreeOfAKind
    it's impossible for two poker hands to have the same three of a kind so
    we do not need to check the 'other cards' in a three of a kind hand and
    also don't need to check if two sets are the same, as there are no 6 of
    one card.

--- lib/poker_hands/hand_rankings/hand_entities/three_of_a_kind.rb
+++ lib/poker_hands/hand_rankings/hand_entities/three_of_a_kind.rb
@@ -14,18 +14,9 @@ module PokerHands
 
       def <=>(other_hand)
         set = @set.map(&:rank)
-        other_cards = @other_cards.map(&:rank).sort.reverse
-
         other_hand_set = other_hand.set.map(&:rank)
-        other_hand_cards = other_hand.other_cards.map(&:rank).sort.reverse
 
-        if (set <=> other_hand_set) != 0
-          return set <=> other_hand_set
-        elsif (other_cards <=> other_hand_cards) != 0
-          return other_cards <=> other_hand_cards
-        else
-          return 'tie'
-        end
+        set <=> other_hand_set
       end
     end
   end
