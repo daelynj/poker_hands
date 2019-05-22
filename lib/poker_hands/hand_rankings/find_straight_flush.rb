ab7bd2f0cda5981a866ba7d0ad90ca427c98a0a0
daelynj <daelyn.jones@juul.com>
2019-05-22 16:27:25 -0700

    Add wheel straight logic to FindStraightFlush
    StraightFlush needs to have a high_card attribute like Straight to
    maintain convention. A wheel straight flush needs to be able to be
    recognized in FindStraightFlush for the proper high_card to be passed.
    This lets us also change the logic for determining ties because we can
    now compare high_card attributes.

--- lib/poker_hands/hand_rankings/find_straight_flush.rb
+++ lib/poker_hands/hand_rankings/find_straight_flush.rb
@@ -8,7 +8,14 @@ module PokerHands
         return nil
       end
 
-      Entities::StraightFlush.new(cards: hand)
+      sorted_hand = hand.sort_by { |card| card.rank }
+      if sorted_hand[-1].rank == 14 && sorted_hand[-2].rank == 5
+        high_card = sorted_hand[-2]
+      else
+        high_card = sorted_hand.last
+      end
+      
+      Entities::StraightFlush.new(cards: hand, high_card: high_card)
     end
   end
 end
--- lib/poker_hands/hand_rankings/hand_entities/straight_flush.rb
+++ lib/poker_hands/hand_rankings/hand_entities/straight_flush.rb
@@ -2,20 +2,21 @@ module PokerHands
   module Entities
     class StraightFlush
       include Comparable
-      attr_reader :cards, :strength, :type
+      attr_reader :cards, :high_card, :strength, :type
 
-      def initialize(cards:)
+      def initialize(cards:, high_card:)
         @type = 'straight flush'
         @cards = cards
+        @high_card = high_card
         @strength = 9
       end
 
       def <=>(other_hand)
-        straight = @cards.map(&:rank).reverse
-        other_hand_straight = straight.map(&:rank).reverse
-        
-        if (straight <=> other_hand_straight) != 0
-          return straight <=> other_hand_straight
+        high_card = @high_card.rank
+        other_hand_high_card = other_hand.high_card.rank
+
+        if (high_card <=> other_hand_high_card) != 0
+          return high_card <=> other_hand_high_card
         else
           return 'tie'
         end
