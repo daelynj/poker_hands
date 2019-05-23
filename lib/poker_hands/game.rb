cf92214547ee056be93f7db301efeed86558aeea
daelynj <daelyn.jones@juul.com>
2019-05-23 15:58:44 -0700

    Create DeclareWinner service object
    DeclareWinner now prints the winning message instead of doing it in
    game.rb. This is valuable because game.rb doesn't need to be tested to
    be printing properly. game.rb only needs to make the call.

--- lib/poker_hands/game.rb
+++ lib/poker_hands/game.rb
@@ -6,16 +6,14 @@ module PokerHands
     def initialize
 
       deck = Deck.new
-      shuffled_deck = deck.shuffle
+      
+      shuffled_deck = deck.clone
+      shuffled_deck.cards.shuffle!
 
       hand1 = shuffled_deck.draw(5)
       hand2 = shuffled_deck.draw(5)
 
-      winning_message = CompareHands.new.call(hand1, hand2)
-      
-      puts "Hand 1: [#{hand1.join(', ')}]"
-      puts "Hand 2: [#{hand2.join(', ')}]"
-      puts "\n#{winning_message}"
+      DeclareWinner.new.call(hand1, hand2, CompareHands.new.call(hand1, hand2))
     end
   end
 end
