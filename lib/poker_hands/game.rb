c05e7625619c12286a75fdbb2a2f0880863b087a
daelynj <daelyn.jones@juul.com>
2019-05-23 13:33:00 -0700

    Add BuildCards factory
    We use a BuildCards factory so that the Deck class no longer knows how
    to build cards itself. This makes testing easier and makes logical
    sense.
    
    For now we will assume that there are 13 ranks and 4 suits as is a
    standard deck of cards that would be used in a game of poker. In the
    future we could add a parameter to Deck that allows us to define what
    a deck, for instance if we wanted suits to be emojis or if we wanted 20
    ranks instead of 13.

--- lib/poker_hands/game.rb
+++ lib/poker_hands/game.rb
@@ -4,15 +4,17 @@ require 'poker_hands/compare_hands'
 module PokerHands
   class Game
     def initialize
+
       deck = Deck.new
+      shuffled_deck = deck.shuffle
 
-      @hand1 = deck.draw(5)
-      @hand2 = deck.draw(5)
+      hand1 = shuffled_deck.draw(5)
+      hand2 = shuffled_deck.draw(5)
 
-      winning_message = CompareHands.new.call(@hand1, @hand2)
+      winning_message = CompareHands.new.call(hand1, hand2)
       
-      puts "Hand 1: [#{@hand1.join(', ')}]"
-      puts "Hand 2: [#{@hand2.join(', ')}]"
+      puts "Hand 1: [#{hand1.join(', ')}]"
+      puts "Hand 2: [#{hand2.join(', ')}]"
       puts "\n#{winning_message}"
     end
   end
