a833d4337ea0dfc5c8769066402b08b83d9c6acf
daelynj <daelyn.jones@juul.com>
2019-05-24 10:54:41 -0700

    Re-implement tie logic for sets

--- lib/poker_hands/hand_rankings/hand_entities/three_of_a_kind.rb
+++ lib/poker_hands/hand_rankings/hand_entities/three_of_a_kind.rb
@@ -13,10 +13,19 @@ module PokerHands
       end
 
       def <=>(other_hand)
+        other_cards = @other_cards.map(&:rank).sort.reverse
         set = @set.map(&:rank)
-        other_hand_set = other_hand.set.map(&:rank)
 
-        set <=> other_hand_set
+        other_hand_set = other_hand.set.map(&:rank)
+        other_hand_cards = other_hand.other_cards.map(&:rank).sort.reverse
+
+        if (set <=> other_hand_set) != 0
+          return set <=> other_hand_set
+        elsif (other_cards <=> other_hand_cards) != 0
+          return other_cards <=> other_hand_cards
+        else
+          return 'tie'
+        end
       end
     end
   end
